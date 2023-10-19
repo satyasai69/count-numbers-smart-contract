4329938b754f274267da339a1cafa73766297fec
satyasai69 <ch.satya.sai133@gmail.com>
2023-10-20 00:59:44 +0530

    damo in live on testnet

--- test-count.sol
+++ test-count.sol
@@ -1,3 +1,11 @@
+//live damo on goerli testnet
+// contract address = 0x26F2584169D03BdF7e6a1B5C95a1abEa336feB74;
+//link ethscan = https://goerli.etherscan.io/address/0x26f2584169d03bdf7e6a1b5c95a1abea336feb74
+
+
+
+
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 contract countnumbers {
     uint public count;
